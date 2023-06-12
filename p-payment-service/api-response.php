e6942ad08714de480b39ecb8b9cdbcffc85718b4
msevimli <muhammetsevimli06@gmail.com>
2023-06-12 02:00:52 +0200

    api-response

---
+++ p-payment-service/api-response.php
@@ -0,0 +1,96 @@
+﻿<?php
+
+header('Content-Type: application/json; charset=utf-8');
+
+none("test");
+function none($mes)
+{
+    //echo json_encode(array("mes" => $mes));
+?>
+{
+    "Name":"John",
+    "Age":34,
+    "StateOfOrigin":"England",
+    "Pets":[
+            {"Type":"Cat","Name":"MooMoo","Age":3.4},
+            {"Type":"Squirrel","Name":"Sandy","Age":7}
+    ],
+    "products": [
+        {
+            "id": 5,
+            "categoryId": [
+                1,2
+            ],
+            "productName": "Fanta Orange (0,33 l)",
+            "description": "",
+            "unitPrice": 25,
+            "image": "https://terminal.plife.se/media/TestComp-48/pngegg (20).png"
+        },
+        {
+            "id": 2,
+            "categoryId": [
+                1,2
+            ],
+            "productName": "Coca Cola (0,33 l)",
+            "description": "",
+            "unitPrice": 25,
+            "image": "https://terminal.plife.se/media/TestComp-48/pngegg (18).png"
+        },
+        {
+            "id": 26,
+            "categoryId": [
+                2
+            ],
+            "productName": "1. Margherita (V)",
+            "description": "",
+            "unitPrice": 69,
+            "image": "https://terminal.plife.se/media/TestComp-48/pngegg (2).png",
+            "additional": [
+                {
+                    "Ekstra Toppings": [
+                        {
+                            "option_name": "Chili",
+                            "price": 0,
+                            "direction": "plus"
+                        },
+                        {
+                            "option_name": "Oregano",
+                            "price": 0,
+                            "direction": "plus"
+                        },
+                        {
+                            "option_name": "Rosmarin",
+                            "price": 0,
+                            "direction": "plus"
+                        },
+                        {
+                            "option_name": "Hvidløgsolie",
+                            "price": 0,
+                            "direction": "plus"
+                        }
+                    ],
+                    "multiple": "true"
+                }
+            ]
+        }
+    ],    
+    "categories": [
+        {
+            "id": 4,
+            "name": "Palace Burger",
+            "image": "https://terminal.plife.se/media/TestComp-48/pngegg (4).png"
+        },
+        {
+            "id": 2,
+            "name": "Pizza",
+            "image": "https://terminal.plife.se/media/TestComp-48/pngegg (2).png"
+        }
+    ],    
+    "settings": {
+        "lastUpdate": "2023-05-29 00:21:20",
+        "storeName": "Burger Palace",
+        "storeLogo": "https://terminal.plife.se/media/TestComp-48/59_homescreenlogo_1024x1024 (1).jpeg"
+    }
+}
+<?php
+}
