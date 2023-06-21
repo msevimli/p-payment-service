bad115d45cc18aa9ee06f1515b26bae92957718c
msevimli <muhammetsevimli06@gmail.com>
2023-06-21 03:26:34 +0200

    pos-integration

--- p-payment-service/Checkout.Designer.cs
+++ p-payment-service/Checkout.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             this.mainPanel = new System.Windows.Forms.Panel();
-            this.topPanel = new System.Windows.Forms.Panel();
-            this.bottomPanel = new System.Windows.Forms.Panel();
-            this.paymentOptionPanel = new System.Windows.Forms.Panel();
             this.paymentFlowPanel = new System.Windows.Forms.Panel();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.bankCard = new System.Windows.Forms.RadioButton();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.backToCart = new System.Windows.Forms.Button();
-            this.payButton = new System.Windows.Forms.Button();
-            this.eatHere = new System.Windows.Forms.RadioButton();
+            this.paymentOptionPanel = new System.Windows.Forms.Panel();
             this.takeAway = new System.Windows.Forms.RadioButton();
+            this.eatHere = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.bankCard = new System.Windows.Forms.RadioButton();
+            this.bottomPanel = new System.Windows.Forms.Panel();
+            this.payButton = new System.Windows.Forms.Button();
+            this.backToCart = new System.Windows.Forms.Button();
+            this.topPanel = new System.Windows.Forms.Panel();
             this.mainPanel.SuspendLayout();
-            this.bottomPanel.SuspendLayout();
             this.paymentOptionPanel.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.bottomPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -58,26 +58,14 @@
             this.mainPanel.Size = new System.Drawing.Size(800, 450);
             this.mainPanel.TabIndex = 0;
             // 
-            // topPanel
+            // paymentFlowPanel
             // 
-            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.topPanel.Location = new System.Drawing.Point(0, 0);
-            this.topPanel.Name = "topPanel";
-            this.topPanel.Size = new System.Drawing.Size(800, 59);
-            this.topPanel.TabIndex = 0;
-            // 
-            // bottomPanel
-            // 
-            this.bottomPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
-            this.bottomPanel.Controls.Add(this.payButton);
-            this.bottomPanel.Controls.Add(this.backToCart);
-            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.bottomPanel.Location = new System.Drawing.Point(0, 391);
-            this.bottomPanel.Name = "bottomPanel";
-            this.bottomPanel.Padding = new System.Windows.Forms.Padding(5);
-            this.bottomPanel.Size = new System.Drawing.Size(800, 59);
-            this.bottomPanel.TabIndex = 1;
+            this.paymentFlowPanel.BackColor = System.Drawing.Color.White;
+            this.paymentFlowPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.paymentFlowPanel.Location = new System.Drawing.Point(335, 59);
+            this.paymentFlowPanel.Name = "paymentFlowPanel";
+            this.paymentFlowPanel.Size = new System.Drawing.Size(465, 332);
+            this.paymentFlowPanel.TabIndex = 3;
             // 
             // paymentOptionPanel
             // 
@@ -92,85 +80,21 @@
             this.paymentOptionPanel.Size = new System.Drawing.Size(335, 332);
             this.paymentOptionPanel.TabIndex = 2;
             // 
-            // paymentFlowPanel
+            // takeAway
             // 
-            this.paymentFlowPanel.BackColor = System.Drawing.Color.White;
-            this.paymentFlowPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.paymentFlowPanel.Location = new System.Drawing.Point(335, 59);
-            this.paymentFlowPanel.Name = "paymentFlowPanel";
-            this.paymentFlowPanel.Size = new System.Drawing.Size(465, 332);
-            this.paymentFlowPanel.TabIndex = 3;
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.radioButton1);
-            this.groupBox1.Controls.Add(this.bankCard);
-            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox1.Location = new System.Drawing.Point(12, 14);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(301, 176);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Payment Options";
-            // 
-            // bankCard
-            // 
-            this.bankCard.AutoSize = true;
-            this.bankCard.Checked = true;
-            this.bankCard.Image = global::p_payment_service.Properties.Resources.visa_master_align;
-            this.bankCard.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.bankCard.Location = new System.Drawing.Point(9, 25);
-            this.bankCard.Name = "bankCard";
-            this.bankCard.Size = new System.Drawing.Size(186, 71);
-            this.bankCard.TabIndex = 0;
-            this.bankCard.TabStop = true;
-            this.bankCard.Text = "Bank/Credit Card";
-            this.bankCard.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.bankCard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.bankCard.UseVisualStyleBackColor = true;
-            this.bankCard.CheckedChanged += new System.EventHandler(this.bankCard_CheckedChanged);
-            // 
-            // radioButton1
-            // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(9, 115);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(75, 24);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Others";
-            this.radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // backToCart
-            // 
-            this.backToCart.BackColor = System.Drawing.Color.DodgerBlue;
-            this.backToCart.Dock = System.Windows.Forms.DockStyle.Left;
-            this.backToCart.FlatAppearance.BorderSize = 0;
-            this.backToCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.backToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.backToCart.ForeColor = System.Drawing.Color.White;
-            this.backToCart.Location = new System.Drawing.Point(5, 5);
-            this.backToCart.Name = "backToCart";
-            this.backToCart.Size = new System.Drawing.Size(329, 49);
-            this.backToCart.TabIndex = 0;
-            this.backToCart.Text = "Back to Cart";
-            this.backToCart.UseVisualStyleBackColor = false;
-            this.backToCart.Click += new System.EventHandler(this.backToCart_Click);
-            // 
-            // payButton
-            // 
-            this.payButton.BackColor = System.Drawing.Color.LimeGreen;
-            this.payButton.Dock = System.Windows.Forms.DockStyle.Right;
-            this.payButton.FlatAppearance.BorderSize = 0;
-            this.payButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.payButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.payButton.ForeColor = System.Drawing.Color.White;
-            this.payButton.Location = new System.Drawing.Point(340, 5);
-            this.payButton.Name = "payButton";
-            this.payButton.Size = new System.Drawing.Size(455, 49);
-            this.payButton.TabIndex = 1;
-            this.payButton.Text = "Pay";
-            this.payButton.UseVisualStyleBackColor = false;
+            this.takeAway.AutoSize = true;
+            this.takeAway.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.takeAway.Image = global::p_payment_service.Properties.Resources.take_away;
+            this.takeAway.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.takeAway.Location = new System.Drawing.Point(217, 212);
+            this.takeAway.Name = "takeAway";
+            this.takeAway.Size = new System.Drawing.Size(95, 71);
+            this.takeAway.TabIndex = 2;
+            this.takeAway.Text = "Take Away";
+            this.takeAway.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.takeAway.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.takeAway.UseVisualStyleBackColor = true;
+            this.takeAway.CheckedChanged += new System.EventHandler(this.takeAway_CheckedChanged);
             // 
             // eatHere
             // 
@@ -189,21 +113,98 @@
             this.eatHere.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
             this.eatHere.UseVisualStyleBackColor = true;
             // 
-            // takeAway
+            // groupBox1
             // 
-            this.takeAway.AutoSize = true;
-            this.takeAway.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.takeAway.Image = global::p_payment_service.Properties.Resources.take_away;
-            this.takeAway.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
-            this.takeAway.Location = new System.Drawing.Point(217, 212);
-            this.takeAway.Name = "takeAway";
-            this.takeAway.Size = new System.Drawing.Size(95, 71);
-            this.takeAway.TabIndex = 2;
-            this.takeAway.Text = "Take Away";
-            this.takeAway.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.takeAway.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.takeAway.UseVisualStyleBackColor = true;
-            this.takeAway.CheckedChanged += new System.EventHandler(this.takeAway_CheckedChanged);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.bankCard);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 14);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(301, 176);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Payment Options";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(9, 115);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(75, 24);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Others";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // bankCard
+            // 
+            this.bankCard.AutoSize = true;
+            this.bankCard.Checked = true;
+            this.bankCard.Image = global::p_payment_service.Properties.Resources.visa_master_align;
+            this.bankCard.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.bankCard.Location = new System.Drawing.Point(9, 25);
+            this.bankCard.Name = "bankCard";
+            this.bankCard.Size = new System.Drawing.Size(186, 71);
+            this.bankCard.TabIndex = 0;
+            this.bankCard.TabStop = true;
+            this.bankCard.Text = "Bank/Credit Card";
+            this.bankCard.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bankCard.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.bankCard.UseVisualStyleBackColor = true;
+            this.bankCard.CheckedChanged += new System.EventHandler(this.bankCard_CheckedChanged);
+            // 
+            // bottomPanel
+            // 
+            this.bottomPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.bottomPanel.Controls.Add(this.payButton);
+            this.bottomPanel.Controls.Add(this.backToCart);
+            this.bottomPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.bottomPanel.Location = new System.Drawing.Point(0, 391);
+            this.bottomPanel.Name = "bottomPanel";
+            this.bottomPanel.Padding = new System.Windows.Forms.Padding(5);
+            this.bottomPanel.Size = new System.Drawing.Size(800, 59);
+            this.bottomPanel.TabIndex = 1;
+            // 
+            // payButton
+            // 
+            this.payButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.payButton.Dock = System.Windows.Forms.DockStyle.Right;
+            this.payButton.FlatAppearance.BorderSize = 0;
+            this.payButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.payButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.payButton.ForeColor = System.Drawing.Color.White;
+            this.payButton.Location = new System.Drawing.Point(340, 5);
+            this.payButton.Name = "payButton";
+            this.payButton.Size = new System.Drawing.Size(455, 49);
+            this.payButton.TabIndex = 1;
+            this.payButton.Text = "Pay";
+            this.payButton.UseVisualStyleBackColor = false;
+            this.payButton.Click += new System.EventHandler(this.payButton_Click);
+            // 
+            // backToCart
+            // 
+            this.backToCart.BackColor = System.Drawing.Color.DodgerBlue;
+            this.backToCart.Dock = System.Windows.Forms.DockStyle.Left;
+            this.backToCart.FlatAppearance.BorderSize = 0;
+            this.backToCart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.backToCart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backToCart.ForeColor = System.Drawing.Color.White;
+            this.backToCart.Location = new System.Drawing.Point(5, 5);
+            this.backToCart.Name = "backToCart";
+            this.backToCart.Size = new System.Drawing.Size(329, 49);
+            this.backToCart.TabIndex = 0;
+            this.backToCart.Text = "Back to Cart";
+            this.backToCart.UseVisualStyleBackColor = false;
+            this.backToCart.Click += new System.EventHandler(this.backToCart_Click);
+            // 
+            // topPanel
+            // 
+            this.topPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.topPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.topPanel.Location = new System.Drawing.Point(0, 0);
+            this.topPanel.Name = "topPanel";
+            this.topPanel.Size = new System.Drawing.Size(800, 59);
+            this.topPanel.TabIndex = 0;
             // 
             // Checkout
             // 
@@ -221,11 +222,11 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Checkout_FormClosed);
             this.Load += new System.EventHandler(this.Checkout_Load);
             this.mainPanel.ResumeLayout(false);
-            this.bottomPanel.ResumeLayout(false);
             this.paymentOptionPanel.ResumeLayout(false);
             this.paymentOptionPanel.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.bottomPanel.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
--- p-payment-service/Checkout.cs
+++ p-payment-service/Checkout.cs
@@ -4,17 +4,25 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using myPOS;
 
 namespace p_payment_service
 {
     public partial class Checkout : Form
     {
+        myPOSTerminal terminal = new myPOSTerminal();
+       
+
         public Checkout()
         {
             InitializeComponent();
+            terminal.ProcessingFinished += ProcessResult;
+            terminal.SetLanguage(Language.English);
+            RequestResult r = terminal.Activate();
         }
 
         private void Checkout_FormClosed(object sender, FormClosedEventArgs e)
@@ -44,7 +52,27 @@ namespace p_payment_service
         private void Checkout_Load(object sender, EventArgs e)
         {
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            terminal.Initialize((string)"COM3"); // This COM number is used as an example
+
         }
-        
+
+        private void payButton_Click(object sender, EventArgs e)
+        {
+
+            //terminal.SetReceiptMode((ReceiptMode)cmbReceiptMode.SelectedItem);
+            RequestResult r = terminal.Purchase(0.1, myPOS.Currencies.EUR, "");
+
+
+        }
+        protected void ProcessResult(ProcessingResult r)
+        {
+            // handle the response here
+            if (r.TranData != null)
+            {
+                // transaction response
+                Console.WriteLine(r);
+            }
+        }
+
     }
 }
